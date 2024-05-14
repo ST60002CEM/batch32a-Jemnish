c2f5f3b82b8a0ea1da14f6fb920add9c471743f6
Jenish Budhathoki <Budathokizone111@gmail.com>
2024-05-14 10:33:05 +0545

    started work in login screen

---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:final_assignment/screen/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      // themes
+      home: LoginScreen(),
+    );
+  }
+}
