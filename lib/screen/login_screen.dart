cddfff615b86bf45deea70e91ab51a86cb2a197d
Jenish Budhathoki <Budathokizone111@gmail.com>
2024-05-16 20:55:55 +0545

    made some changes in login page

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,92 +1,104 @@
 import 'package:final_assignment/screen/dashboard_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Screen'),
-      ),
       body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Login Page',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Image.asset(
+              'assets/images/logo.png', // Change to your app logo image path
+              width: 100,
+              height: 100,
+            ),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const DashboardScreen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 40,
+                    vertical: 16), // Adjust padding as per your need
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(
+                      8), // Adjust border radius as per your need
+                ),
+                backgroundColor: const Color.fromARGB(
+                    255, 102, 200, 105), // Change background color
+              ),
+              child: const Text('Continue'),
+            ),
+            const SizedBox(height: 10),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.3,
-                  child: Image.asset(
-                    'assets/images/logo.png',
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Form(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'Email',
-                          prefixIcon: Icon(Icons.email),
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Please enter your email';
-                          } else if (value != 'admin') {
-                            return 'Email not registered';
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 20),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'Password',
-                          prefixIcon: Icon(Icons.lock),
-                        ),
-                        obscureText: true,
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Please enter your password';
-                          } else if (value != 'admin') {
-                            return 'Incorrect password';
-                          }
-                          return null;
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
+                TextButton(
                   onPressed: () {
-                    if (formKey.currentState!.validate()) {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const DashboardScreen(),
-                        ),
-                      );
-                    }
+                    // Implement forgot password functionality
                   },
-                  child: const Text('Login'),
+                  child: const Text('Forgot Password?'),
                 ),
-                const SizedBox(height: 20),
-                OutlinedButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.mail),
-                  label: const Text('Sign in with Google'),
+                const SizedBox(width: 10),
+                TextButton(
+                  onPressed: () {
+                    // Implement registration functionality
+                  },
+                  child: const Text('Register'),
                 ),
               ],
             ),
-          ),
+            const SizedBox(height: 20),
+            OutlinedButton.icon(
+              onPressed: () {
+                // Implement login with Google functionality
+              },
+              icon: SvgPicture.asset(
+                'assets/icons/google.svg', // Change to your Google logo image path
+                width: 24,
+                height: 24,
+              ),
+              label: const Text('Login with Google'),
+            ),
+          ],
         ),
       ),
     );
