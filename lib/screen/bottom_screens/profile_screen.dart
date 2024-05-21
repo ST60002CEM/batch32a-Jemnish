0530f192b2bbddab5bd8895c713b16566de0f6e2
Jenish Budhathoki <Budathokizone111@gmail.com>
2024-05-21 22:48:13 +0545

    aligned the profile column to center

--- lib/screen/bottom_screens/profile_screen.dart
+++ lib/screen/bottom_screens/profile_screen.dart
@@ -5,12 +5,10 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             const SizedBox(height: 20),
             const CircleAvatar(
